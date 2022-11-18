b19439a54cda140ec3296b95c68833edcf3168dd
Iryna Aleksandrova <110558905+IreneAleks@users.noreply.github.com>
2022-11-18 23:35:12 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+// Swiper
+
+const swiper = new Swiper('.swiper', {
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 4,
+    loop: true,
+
+  
+  });
