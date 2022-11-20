5c8a8684f4869c0265adce9b05058d72d132af5c
Iryna Aleksandrova <110558905+IreneAleks@users.noreply.github.com>
2022-11-20 20:11:59 +0200

    Update app.js
    add animation to arrow-up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-
-
 // Swiper
 
 const swiper = new Swiper('.swiper', {
@@ -11,6 +9,25 @@ const swiper = new Swiper('.swiper', {
 
     slidesPerView: 4,
     loop: true,
-
-  
   });
+
+
+// Arrow Up
+
+$(window).scroll(function() {
+  let scrolled = $(window).scrollTop();
+
+  if (scrolled > 300) {
+    $('#arrow-up').addClass('active');
+  } else {
+    $('#arrow-up').removeClass('active');
+  }
+});
+
+$('#arrow-up').click(function() {
+  $('body,html').animate({scrollTop: 0}, 1000)
+});
+
+
+
+
